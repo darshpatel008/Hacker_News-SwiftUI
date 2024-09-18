f058024db2b52637feaca0ed94f35a1e6be06d30
darshviroja008 <darshviroja12345@gmail.com>
2024-09-18 11:34:58 +0530

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Darsh viroja  on 18/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
